4b323a2cfe1f301ebd3b42b4968c677516aa1548
Mamadou Korka Diallo <lelevekorka@gmail.com>
2025-01-16 22:40:04 +0100

    Add project files.

---
+++ Animal_management_with_C_Shap/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Animal_management_with_C_Shap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
